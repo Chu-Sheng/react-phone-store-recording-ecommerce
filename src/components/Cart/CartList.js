ef4ae3ca20fbcad592cd7a9d7895fedd3dcd0b07
Chu-Sheng <cooper.juan@yahoo.com>
2021-09-11 14:18:16 -0400

    first commit

---
+++ src/components/Cart/CartList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CartItem from './CartItem';
+
+export default function CartList({value}) {
+  const {cart} = value;
+  // console.log('CartList.js value: ', value);
+  // console.log('CartList.js cart: ', cart);
+  return (
+    <div className="container-fluid">
+      {cart.map(item => {
+        return <CartItem key={item.id} item={item} value={value}/>
+      })}
+    </div>
+  )
+}
